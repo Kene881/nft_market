65f64ed8f908655a040cb0c9c3c2f8ab14929237
NjrexUI <84075064+NjrexUI@users.noreply.github.com>
2022-06-17 22:54:20 +0600

    Added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_nft_market/main.dart';
+import 'package:nftapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
